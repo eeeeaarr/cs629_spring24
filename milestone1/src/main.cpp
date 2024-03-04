3928085d96ecaa1d87ded1b68f8e0a79f61c7181
Jason Scherer <jason.scherer@gmail.com>
2024-03-03 21:47:42 -0500

    Added milestone 1 files but no README yet.

---
+++ milestone1/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "No framework files provided..." << std::endl <<
+    "You're on your own with this one." <<  std::endl <<
+    "Good luck!" << std::endl;
+}
